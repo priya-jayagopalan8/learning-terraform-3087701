7a841937b5d67ca0d28aafe559cd081f6d82f64a
priya-jayagopalan8 <priya.jayagopalan8@gmail.com>
2026-04-08 12:45:54 -0700

    fix typo

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_instance" "web" {
 
 resource "aws_security_group" "web" {
   name        = "web"
-  desciption  = "Allow http and https in. Allow everything out"
+  description  = "Allow http and https in. Allow everything out"
 
   vpc_id = data.aws_vpc.default.id
 }
